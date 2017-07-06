abd2c1e955d20286d9c3d73e3281b19f6e3eca87
boyuanHuang <huang379428449@126.com>
2017-07-06 17:27:40 +0800

    basic fun with bugsssss

--- Main.py
+++ Main.py
@@ -3,32 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def main():
-    img = np.array(Image.open('D:/Temp/205.jpg'))  # 打开图像并转化为数字矩阵
-    rects = [];
-    rows, cols, div = img.shape
-    for i in range(rows):
-        lst_point = [0, 255, 0, [i, -1]]
-        for j in range(cols):
-            current_point = [img[i, j][0], img[i, j][1], img[i, j][2], [i, j]]
-            if __is_new_img_point(lst_point, current_point) is True:
-                if __is_point_in_exist_rect__(current_point, rects) is False:
-                    rects.append(__find_rect_by_point__(current_point,img))
-            lst_point = current_point
-
-
-
-    plt.figure("dog")
-    plt.imshow(img)
-
-    plt.axis('off')
-    plt.show()
-
-
-if __name__ == "__main__":
-    main()
-
-
 def __is_point_in_exist_rect__(point, rects):
     # 判断该点是否是在一个已知的矩形list里 矩形为一个turple,前两个为坐标点，后两个为width height
     for rect in rects:
@@ -38,33 +12,35 @@ def __is_point_in_exist_rect__(point, rects):
         y2 = y1 - rect[3]
         if x1 <= point[3][0] <= x2 and y1 <= point[3][1] <= y2:
             return True
-        else:
-            return False
+    return False
 
 
-def __find_rect_by_point__(i, j, img_array):
+def __find_rect_by_point__(curr_point, img_array):
+    i = curr_point[3][0]
+    j = curr_point[3][1]
     # 当确定了i,j是左上角点后，求出该矩形的长宽各占多少像素点
     rect_width = 1
     rect_height = 1
     rows, cols, div = img_array.shape
     last_point = [img_array[i, j][0], img_array[i, j][1], img_array[i, j][2]]
     for row in range(i+1, rows):
-        current_point = [img_array[row, j]]
-        if __is_green_point__(last_point) is False and __is_green_point__(current_point) is True:
-            break
-        else:
-            rect_width = rect_width + 1
-    last_point = [img_array[i, j][0], img_array[i, j][1], img_array[i, j][2]]
-    for col in range(j+1, cols):
-        current_point = [img_array[i, col]]
+        current_point = img_array[row, j]
         if __is_green_point__(last_point) is False and __is_green_point__(current_point) is True:
             break
         else:
             rect_height = rect_height + 1
-    return rect(i, j, rect_width, rect_height)
+    last_point = [img_array[i, j][0], img_array[i, j][1], img_array[i, j][2]]
+    for col in range(j+1, cols):
+        current_point = img_array[i, col]
+        if __is_green_point__(last_point) is False and __is_green_point__(current_point) is True:
+            break
+        else:
+            rect_width = rect_width + 1
+    rect = (i, j, rect_width, rect_height)
+    return rect
 
 
-def __is_new_img_point(last_point,next_point):
+def __is_new_img_point(last_point, next_point):
     if __is_green_point__(last_point) is True and __is_green_point__(next_point) is False:
         return True
     else:
@@ -76,3 +52,28 @@ def __is_green_point__(point):
         return True
     else:
         return False
+
+
+def main():
+    img = np.array(Image.open('D:/Temp/testImg.bmp'))  # 打开图像并转化为数字矩阵
+    rects = [];
+    rows, cols, div = img.shape
+    for i in range(rows):
+        lst_point = [0, 255, 0, [i, -1]]
+        for j in range(cols):
+            current_point = [img[i, j][0], img[i, j][1], img[i, j][2], [i, j]]
+            if __is_new_img_point(lst_point, current_point) is True:
+                if __is_point_in_exist_rect__(current_point, rects) is False:
+                    rects.append(__find_rect_by_point__(current_point, img))
+            lst_point = current_point
+    plt.figure("dog")
+    plt.imshow(img)
+
+    plt.axis('off')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+
+
